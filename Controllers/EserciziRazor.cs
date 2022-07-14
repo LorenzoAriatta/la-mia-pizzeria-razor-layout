280e54531b26ca1a20e575a83521669b08ebb29f
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-07-14 14:27:23 +0200

    Create new controller EserciziRazor

---
+++ Controllers/EserciziRazor.cs
@@ -0,0 +1,21 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class EserciziRazor : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public EserciziRazor(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
