0c0235a42d2dcd2d4b25a0fc4b30bcd95e07503b
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-07-14 14:12:08 +0200

    Aggiungere i file di progetto.

---
+++ Pizza.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Pizza
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public string Image { get; set; }
+
+    public double Price { get; set; }
+
+    public int ID { get; set; }
+    public Pizza(string name, string description, string image, double price, int iD)
+    {
+        Name = name;
+        Description = description;
+        Image = image;
+        Price = price;
+        ID = iD;    
+    }
+}
