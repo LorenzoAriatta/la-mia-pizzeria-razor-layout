c78f5cae45f4ddbf861c1c48ddeba50c8d02a07d
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-07-14 17:38:39 +0200

    create new class

---
+++ PizzaList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class PizzaList
+{
+	List<Pizza> pizze;
+	public PizzaList()
+	{
+		this.pizze = new List<Pizza>();
+	}
+
+	public void AddToList(Pizza pizza)
+    {
+		pizze.Add(pizza);
+    }
+
+	public List<Pizza> GetList()
+    {
+		return pizze;
+    }
+}
